4645b9f1c850ee3397580c9f92f4ba6f5747af33
Adam Ladell <adamladell@outlook.com>
2019-07-10 12:21:28 +1000

    Set up route index

--- api.js
+++ api.js
@@ -6,10 +6,13 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
+/* Require Routes */
+const ROUTES = require('./routes/');
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/hello', (req, res) => res.send('Hello, World!'));
+app.use('/', ROUTES);
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, (err) => {
   console.log(err || 'Connected to MongoDB');
