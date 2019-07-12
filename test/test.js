bfd6518e62c051295fbab8669c57d815768db6f5
Adam Ladell <adamladell@outlook.com>
2019-07-12 11:15:56 +1000

    Added supertest package

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const should = require('chai').should();
-describe('Test', function() {
-  it('should equal 2', function() {
-    const x = 2;
-
-    x.should.equal(2);
-  });
-});
---
+++ test/unit-tests/auth/registration.js
@@ -0,0 +1,1 @@
+const should = require('chai').should();
